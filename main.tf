0c4e788e6d4505acbc08c338afeb41d45644c1ee
MaxAndreev27 <maxandreev27@gmail.com>
2024-09-11 17:59:29 +0300

    Change region from us-east-2 to eu-central-1 and ami

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "eu-central-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0fb653ca2d3203ac1"
+  ami           = "ami-0cee4a3eca5195216"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
   user_data = <<-EOF
