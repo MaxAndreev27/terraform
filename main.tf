5898718ef31d46af74818d425ee07c3fdef1d3f0
MaxAndreev27 <maxandreev27@gmail.com>
2024-09-13 17:28:51 +0300

    t2.micro

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_launch_configuration" "example" {
   image_id        = "ami-0cee4a3eca5195216"
-  instance_type   = "t2.nano"
+  instance_type   = "t2.micro"
   security_groups = [aws_security_group.instance.id]
 
   user_data       = <<-EOF
